953767bcf884babae9c18f6484d8c4aa60b077ed
raidan PC <raidan00@mail.ru>
2024-04-14 21:12:14 +0300

    add select lvl

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
 	import Game from './Game.svelte';
+	import SelectLevel from './SelectLevel.svelte';
+	import { route } from './store.js';
 </script>
 
-<Game/>
+{#if $route.route == 'select level'}
+	<SelectLevel/>
+{:else}
+	<Game/>
+{/if}
 
---
+++ src/SelectLevel.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { route } from './store.js';
+
+	function reset() {
+		route.set(0);
+	}
+</script>
+
+<button on:click={reset}>
+	start game
+</button>
+
